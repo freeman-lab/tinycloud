418f1986eaae4421fb70c8db07f4faaefdfe7b87
freeman <the.freeman.lab@gmail.com>
2015-10-10 00:33:03 -0400

    Change name of example

---
+++ example.js
@@ -0,0 +1,22 @@
+var tinycloud = require('./index.js')
+
+var options = {
+  dry: false,
+  image: 'ami-d05e75b8',
+  type: 'm3.medium',
+  cluster: 'voltron',
+  ports: [22, 80],
+  key: 'mykey'
+}
+
+var groups = [
+  {tag: 'master', count: 1},
+  {tag: 'worker', count: 1}
+]
+
+var cloud = new tinycloud(options, groups)
+
+cloud.launch( function(err, data) {
+  if (err) console.log(err)
+  if (data) console.log(data)
+})
